db64fef9c59228ab4cbbf3b64d6cedc41b2dad91
crystaljobe <crystaljobe29@gmail.com>
2024-01-31 21:29:13 -0700

    created linear & global search functions

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,21 @@
 function linearSearch(searchTerm, arr) {
-  return undefined;
+  if (arr.indexOf(searchTerm) === -1) {
+    return undefined;
+  } else {
+    return arr.indexOf(searchTerm);
+  }
 }
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  let indexArr = [];
+  let currIndex = arr.indexOf(searchTerm);
+  
+  while (currIndex !== -1) {
+    indexArr.push(currIndex);
+    currIndex = arr.indexOf(searchTerm, currIndex + 1);
+  }
+
+  return indexArr.length > 0 ? indexArr : undefined;
 }
 
 module.exports = { linearSearch, globalLinearSearch };
